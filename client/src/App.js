8985a7a122f2b07970c2082e108b7e8b51a65f25
Floella Otudeko <ella@Floellas-Air.fritz.box>
2023-03-23 11:57:39 +0000

    React app connected to socket.io server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect('http://localhost:4000');
+
 
 function App() {
   return (
