59f8204cee06bbeb9a68be5e4ec312a3fa4715e5
Floella Otudeko <ella@Floellas-Air.fritz.box>
2023-03-23 11:42:06 +0000

    real time connection established with react app

--- server/index.js
+++ server/index.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+// Imports HTTP and CORS library
+const http = require('http').Server(app);
+const cors = require('cors');
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+// Creates real-time connenction with the react app
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+});
+
 app.get('/api', (req, res) => {
   res.json({
     message: 'Hello world',
